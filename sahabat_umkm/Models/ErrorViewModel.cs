992c6d72f5e434323c4f635eb6ce3da6ec5457c5
aguspradikta <aguspradikta@gmail.com>
2017-10-20 09:54:51 +0700

    Add project files.

---
+++ sahabat_umkm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sahabat_umkm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
